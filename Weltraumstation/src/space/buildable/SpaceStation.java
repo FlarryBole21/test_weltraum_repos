3251b3faa8c029e37ea7920b7eec029383ecc12d
Dennis <161702553+FlarryBole21@users.noreply.github.com>
2024-04-19 06:06:48 +0200

    Update 36 - Anpassen der SpaceStaion

--- Weltraumstation/src/space/buildable/SpaceStation.java
+++ Weltraumstation/src/space/buildable/SpaceStation.java
@@ -1,5 +1,12 @@
 package space.buildable;
 
+import java.util.LinkedList;
+
+import space.buildable.ship.BattleShip;
+import space.buildable.ship.CargoShip;
+import space.buildable.ship.ColonyShip;
+import space.inventory.item.Item;
+
 public class SpaceStation extends Building{
 	
 	private String name;
@@ -25,6 +32,23 @@ public class SpaceStation extends Building{
 	@Override
 	public void upgrade() {
 		
+	}
+	
+    public BattleShip craftBattleShip(LinkedList<Item> items, int amount){
+		
+		return new BattleShip(1000,750,20);
+		
+	}
+	
+	public ColonyShip craftColonyShip(LinkedList<Item> items, int amount){
+		
+		return new ColonyShip(1000,50);
+		
+	}
+	
+	public CargoShip craftCargoShip(LinkedList<Item> items, int amount){
+		
+		return new CargoShip(1000,50);
 	}
 
 }
--- Weltraumstation/src/space/buildable/ship/Ship.java
+++ Weltraumstation/src/space/buildable/ship/Ship.java
@@ -6,13 +6,11 @@ public abstract class Ship extends Vehicle{
 	
 	private int defense;
 	private int capacity;
-	private String condition;
 	private int level;
 	
 	public Ship (int defense, int capacity) {
 		this.defense = defense;
 		this.capacity = capacity;
-		this.condition = "voll funktionsfähig";
 		this.level = 1;
 	}
 
